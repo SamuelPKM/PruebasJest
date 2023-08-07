1e023cd6fb88f09cbadb31611dc53b3c61deb027
Samuel R Andrade <samuel11299ac@gmail.com>
2023-08-07 12:31:40 -0600

    algunos test de ejemplo

--- Tests/sum.test.js
+++ Tests/sum.test.js
@@ -40,20 +40,81 @@ test("cero", () => {
   expect(z).toBeFalsy();
 });
 
-test('dos mas dos', () => {
-    const value = 2 + 2;
-    expect(value).toBeGreaterThan(3);
-    expect(value).toBeGreaterThanOrEqual(3.5);
-    expect(value).toBeLessThan(5);
-    expect(value).toBeLessThanOrEqual(4.5);
-  
-    // toBe y toEqual son equivalentes para números
-    expect(value).toBe(4);
-    expect(value).toEqual(4);
-  });
+test("dos mas dos", () => {
+  const value = 2 + 2;
+  expect(value).toBeGreaterThan(3);
+  expect(value).toBeGreaterThanOrEqual(3.5);
+  expect(value).toBeLessThan(5);
+  expect(value).toBeLessThanOrEqual(4.5);
 
-  test('agregando números de punto flotante', () => {
-    const value = 0.1 + 0.2;
-    //expect(value).toBe(0.3); Esto no funcionará debido al error de redondeo
-    expect(value).toBeCloseTo(0.3); // Esto funciona.
+  // toBe y toEqual son equivalentes para números
+  expect(value).toBe(4);
+  expect(value).toEqual(4);
+});
+
+test("agregando números de punto flotante", () => {
+  const value = 0.1 + 0.2;
+  //expect(value).toBe(0.3); Esto no funcionará debido al error de redondeo
+  expect(value).toBeCloseTo(0.3); // Esto funciona.
+});
+
+test("no hay I en Team", () => {
+  expect("team").not.toMatch(/I/);
+});
+
+test('hay "stop" en Christoph', () => {
+  expect("Christoph").toMatch(/stop/);
+});
+
+function compileAndroidCode() {
+  throw new Error("you are using the wrong JDK!");
+}
+
+test("compiling android goes as expected", () => {
+  expect(() => compileAndroidCode()).toThrow();
+  expect(() => compileAndroidCode()).toThrow(Error);
+
+  // You can also use a string that must be contained in the error message or a regexp
+  expect(() => compileAndroidCode()).toThrow("you are using the wrong JDK");
+  expect(() => compileAndroidCode()).toThrow(/JDK/);
+
+  // Or you can match an exact error message using a regexp like below
+  expect(() => compileAndroidCode()).toThrow(/^you are using the wrong JDK$/); // Test fails
+  expect(() => compileAndroidCode()).toThrow(/^you are using the wrong JDK!$/); // Test pass
+});
+
+const fetchData = () => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('peanut butter');
+        }, 1000);
+    });
+};
+
+test("el dato es peanut butter", () => {
+  return fetchData().then((data) => {
+    expect(data).toBe("peanut butter");
   });
+});
+
+test("el dato es peanut butter", async () => {
+  const data = await fetchData();
+  expect(data).toBe("peanut butter");
+});
+
+test("the fetch fails with an error", async () => {
+  expect.assertions(1);
+  try {
+    await fetchData();
+  } catch (e) {
+    expect(e).toMatch("error");
+  }
+});
+
+test("el dato es peanut butter", async () => {
+  await expect(fetchData()).resolves.toBe("peanut butter");
+});
+
+test("el fecth falla con un error", async () => {
+  await expect(fetchData()).rejects.toMatch("error");
+});
